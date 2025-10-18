631d73d36ce233b3cabc70dccebfe234ba530cc1
sdsiridia <pablorossa@gmail.com>
2025-10-18 10:28:51 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base:"/headspa1/",
     base: "./", //al agreagar el . usa rutas relativas  en los enlaces generados por vite
 });
